a7f873c19ba7d5dc278c605c1b19df26eb7027bf
Sarah Kimak <sarahekimak@gmail.com>
2022-02-09 11:09:02 -0500

    task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,7 +8,51 @@ function App(): JSX.Element {
             <header className="App-header">
                 Sarah Kimak UD CISC275 with React Hooks and TypeScript
             </header>
-            <p> Hello World </p>
+            <h1>All About Bailey</h1>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        Meet my dog Bailey. She is almost six years old
+                        retreiver. Bailey likes to:
+                        <ul>
+                            <li>Run around fields on Sunday Fun-day</li>
+                            <li>Eat sticks, scrambled eggs and treats</li>
+                            <li>Get pets and endless attention</li>
+                        </ul>
+                        <div
+                            style={{
+                                border: "1px solid red",
+                                padding: "4px",
+                                width: "100px",
+                                height: "30px",
+                                backgroundColor: "#ff0000"
+                            }}
+                        >
+                            rectangle!
+                        </div>
+                    </Col>
+                    <Col>
+                        <img
+                            src="file:///Users/sarahk415/Desktop/tasks/image/bailey.png"
+                            alt="A picture of Bailey in snow."
+                        />
+                        <div
+                            style={{
+                                border: "1px solid red",
+                                padding: "4px",
+                                width: "100px",
+                                height: "30px",
+                                backgroundColor: "#ff0000"
+                            }}
+                        >
+                            rectangle!
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
