dab3ed57adfe2fdc6393d7a62bdd53cabb91997c
Sarah Kimak <sarahekimak@gmail.com>
2022-03-05 21:01:57 -0500

    reveal and change

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,19 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [qType, setqType] = useState<QuestionType>("short_answer_question");
+    const changeState = () => {
+        if (qType === "short_answer_question") {
+            setqType("multiple_choice_question");
+        } else {
+            setqType("short_answer_question");
+        }
+    };
+    return (
+        <div>
+            <Button onClick={changeState}>Change Type</Button>
+            {qType === "multiple_choice_question" && <div>Multiple Choice</div>}
+            {qType === "short_answer_question" && <div>Short Answer</div>}
+        </div>
+    );
 }
