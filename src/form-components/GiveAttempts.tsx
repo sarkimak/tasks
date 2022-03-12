fc963f79509ff0101099db614cd606d37eb3acaa
Sarah Kimak <sarahekimak@gmail.com>
2022-03-12 14:05:36 -0500

    edit mode from class

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsReq. setAttemptsReq] = useState<number>(0);
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId=requestedAttempts">
         </div>
     );
 }
