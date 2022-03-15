f5e350c12bb086ac61acdc1acdde963e882096f5
Sarah Kimak <sarahekimak@gmail.com>
2022-03-15 16:40:35 -0400

    list of req

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the course name somewhere", () => {
+test("There is a quizzer header", () => {
     render(<App />);
-    const linkElement = screen.getByText(/CISC275/i);
+    const linkElement = screen.getByText(/Quizzer/i);
     expect(linkElement).toBeInTheDocument();
+});
+test("There is an image of the sketch", () => {
+    render(<App />);
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+});
+
+test("There is a list of requirements", () => {
+    render(<App />);
+    const list = screen.getByRole("list");
+    expect(list).toBeInTheDocument();
+    expect(list.children.length).toBeGreaterThanOrEqual(3);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,23 @@ function App(): JSX.Element {
                 <img src={require("./sketch.png")} />
             </div>
             <hr></hr>List of Completed Requirements<hr></hr>
+            <ul>
+                <li>Application is Sketched: Y Tested: N</li>
+                <li>Quizzes are Visible: Y Tested: N</li>
+                <li>Quizzes have Questions: Y Tested: N</li>
+                <li>Short Answer and Multiple Choice: Y Tested: N</li>
+                <li>Check Correctness: Y Tested: N</li>
+                <li>Sum Points: Y Tested: N</li>
+                <li>Clear Answers: Y Tested: N</li>
+                <li>Publish Questions: Y Tested: N</li>
+                <li>Filter Questions: Y Tested: N</li>
+                <li>Edit Questions: Y Tested: N</li>
+                <li>Add Questions: Y Tested: N</li>
+                <li>Delete Questions: Y Tested: N</li>
+                <li>Reorder Questions: Y Tested: N</li>
+                <li>Add Quizzes: Y Tested: N</li>
+                <li>Delete Quizzes: Y Tested: N</li>
+            </ul>
         </div>
     );
 }
