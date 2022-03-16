14a19b44708bcda24d7619f1498d04e58e93ac7c
Sarah Kimak <sarahekimak@gmail.com>
2022-03-15 20:55:58 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App(): JSX.Element {
-/*    const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
-
-    function editQuiz(id: string, newQuiz: Quiz) {
-        setQuizzes(
-            Quiz.map(
-                (quizzes: Quiz): Quiz => (quizzes.id === id ? newQuiz : quizzes)
-            )
-        );
-    }
-
-    function deleteQuiz(id: string) {
-        setQuizzes(
-            quizzes.filter((quizzes: Quiz): boolean => quizzes.id !== id)
-        );
-    }
-*/
     return (
         <div className="App">
             <header className="App-header">Quizzer</header>
-            <div>
-                <QuizList
-                    quizzes={quizzes}
-                    editQuiz={editQuiz}
-                    deleteQuiz={deleteQuiz}
-                ></QuizList>
-            </div>
             <hr></hr>Sketch<hr></hr>
             <div className="sketch_img">
                 <img src={require("./sketch.png")} />
             </div>
-            <hr></hr>List of Completed Requirements<hr></hr>
+            <hr></hr>List of Completed Requirements:<hr></hr>
             <ul>
                 <li>Application is Sketched: Y Tested: N</li>
                 <li>Quizzes are Visible: Y Tested: N</li>
