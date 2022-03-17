16c2999438fa14aa11eea648e1b756d373766dfb
Sarah Kimak <sarahekimak@gmail.com>
2022-03-17 15:55:34 -0400

    initial quiz view

--- src/quizzer/components/QuizView.tsx
+++ src/quizzer/components/QuizView.tsx
@@ -1,3 +1,21 @@
 import React from "react";
 import { Quiz } from "../../interfaces/question.ts";
 import { Button, Form } from "react-bootstrap";
+
+export function QuizView({ quiz }: { quiz: Quiz }): JSX.Element {
+    return (
+        <><div>
+            <QuizList
+                quizzes={quizzes} />
+            <div>
+                <StartQuizB currQ={currQ} setMode={setMode} />
+                {"   "}
+                <EdittQuizB currQ={currQ} setMode={setMode} />
+            </div>
+            <br></br>
+            <p>{"Add Quiz"}</p>
+        </div><NewQuizB setMode={setMode} /></>
+        </div>
+    </div>
+    );
+}
