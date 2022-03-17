0415faf33f1340cac868474605525be199d054bb
Sarah Kimak <sarahekimak@gmail.com>
2022-03-17 14:36:45 -0400

    changing quizzer consts

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,4 +1,28 @@
 import React from "react";
+import { Quiz } from "../interfaces/quizzes";
+import { Question, QuestionType } from "../interfaces/questions";
+import quest from "./data/quizquestions.json";
+
+const QUIZZES: Quiz[] = quest.map(
+    (quizzes): Quiz => ({
+        ...quizzes,
+        questions: quizzes.questions.map(
+            (questions): Question ({
+                ...questions,
+                type: questions.type as QuestionType
+            })
+        )
+    })
+);
+
+const NEW_QUIZ: Quiz = {
+    id: 10,
+    title: "New Blank Quiz",
+    points: 0,
+    description: "",
+    maxQ: 0,
+    questions: []
+};
 
 export function Quizzer(): JSX.Element {
     return <h3>Quizzer</h3>;
