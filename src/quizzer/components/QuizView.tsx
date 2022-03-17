384585be4d9c6302fb1913f82d186a3660893930
Sarah Kimak <sarahekimak@gmail.com>
2022-03-17 16:24:27 -0400

    shakeout

--- src/quizzer/components/QuizView.tsx
+++ src/quizzer/components/QuizView.tsx
@@ -1,6 +1,38 @@
 import React from "react";
-import { Quiz } from "../../interfaces/question.ts";
+import { Quiz } from "../../interfaces/quiz";
 import { Button, Form } from "react-bootstrap";
+
+function QuizList({
+    quizzes,
+    currQ,
+    setCurrQ
+}: {
+    quizzes: Quiz[];
+    currQ: Quiz;
+    setCurrQ: (newQuiz: Quiz) => void;
+}): JSX.Element {
+    function changeCurrQ(event: React.ChangeEvent<HTMLSelectElement>){
+        const id = parseInt(event.target.value);
+    }
+
+return (
+    <div>
+        <Form.Group controlId="quizList">
+            <Form.Select value={currQ.id} onChange={changeCurrQ}>
+                {quizzes.map(
+                    (q: Quiz): JSX.Element => (
+                        <option key={q.id} value={q.id}>
+                                {q.title}
+                            </option>
+                    )
+                )}
+                </Form.Select>
+            </Form.Group>
+            <p>{currQ.points}</p>
+            <p>{currQ.description}</p>
+            </div>
+);
+                    }
 
 export function QuizView({ quiz }: { quiz: Quiz }): JSX.Element {
     return (
