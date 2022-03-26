d8d8181d0ec67b6bdcb5bf156b2d8b50eed99032
Sarah Kimak <sarahekimak@gmail.com>
2022-03-26 15:36:24 -0400

    interfacessss

--- src/interfaces/quiz.ts
+++ src/interfaces/quiz.ts
@@ -3,8 +3,7 @@ import { Question } from "./question";
 export interface Quiz {
     id: number;
     title: string;
-    points: number;
     description: string;
-    maxQ: number;
+    open: boolean;
     questions: Question[];
 }
