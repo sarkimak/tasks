29dfbb9b917e0e5a8bdd1d2d90de741e43082944
Sarah Kimak <sarahekimak@gmail.com>
2022-03-26 16:32:09 -0400

    add quiz modal / movie help / switch quiz view around

---
+++ src/quizzer/components/AddQuizModal.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Button, Modal, Form, Col, Row } from "react-bootstrap";
+import { Quiz } from "../../interfaces/quiz";
+
+export function AddQuizModal({
+    show,
+    handleClose,
+    addQuiz
+}: {
+    show: boolean;
+    handleClose: () => void;
+    addQuiz: (newQuiz: Quiz) => void;
+}) {
+    const [id, setId] = useState<number>(3);
+
+    function saveChanges() {
+        addQuiz({
+            id: id,
+            title: "",
+            description: "",
+            questions: [],
+            open: false
+        });
+        handleClose();
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add New Quiz</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {/* Title */}
+                <Form.Group controlId="formQuizeId" as={Row}>
+                    <Form.Label column sm={3}>
+                        Quiz ID:
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            value={id}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setId(parseInt(event.target.value))}
+                        />
+                    </Col>
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={saveChanges}>
+                    Save Quiz
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
