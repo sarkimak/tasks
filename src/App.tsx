142ea82cd459d15c13fe83c32a2adb5aa2867203
Sarah Kimak <sarahekimak@gmail.com>
2022-03-27 15:40:35 -0400

    quizzer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Quizzer } from "./quizzer/Quizzer";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Quizzer</header>
+            <Quizzer></Quizzer>
             <hr></hr>Sketch<hr></hr>
             <div className="sketch_img">
                 <img src={require("./sketch.png")} />
